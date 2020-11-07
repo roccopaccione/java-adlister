6e0eca6be37800f55a476da5bf96eaf06f91a044
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-11-06 22:42:16 -0600

    feat: Cleaned up my code on the ViewProfileServlet and login.jsp

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -11,7 +11,8 @@ public class ViewProfileServlet extends HttpServlet {
         if(request.getSession().getAttribute("user") == null){
             response.sendRedirect("/login");
             return;
+        }else{
+            request.getRequestDispatcher("/profile.jsp").forward(request, response);
         }
-        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }

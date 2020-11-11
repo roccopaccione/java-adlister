9f26182b2db7b7a7d7046e3648099deebf8fdfd3
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-11-10 18:21:53 -0600

    No changes made

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,8 +1,11 @@
 package com.codeup.adlister.dao;
 
+
+import sun.security.krb5.Config;
+
 public class DaoFactory {
     private static Ads adsDao;
-    private static Config config = new Config();
+    private static final Config config = new Config();
 
     public static Ads getAdsDao() {
         if (adsDao == null) {

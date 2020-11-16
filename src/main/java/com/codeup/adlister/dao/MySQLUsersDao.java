e24ee0a272f80b7579a213f42e1ecf4e6b996eea
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-11-16 10:03:17 -0600

    This commmit finsihes all the exercises for passwords

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -3,6 +3,7 @@ package com.codeup.adlister.dao;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
 
+import javax.servlet.jsp.jstl.core.Config;
 import java.sql.*;
 
 public class MySQLUsersDao implements Users {
